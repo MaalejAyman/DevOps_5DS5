7bdd76b8f9fcfcca236b779c0f166c068a9b53fa
MaalejAyman <78954935+MaalejAyman@users.noreply.github.com>
2022-10-30 18:18:11 +0100

    Update ProduitRestController.java

--- Spring/src/main/java/com/esprit/examen/controllers/ProduitRestController.java
+++ Spring/src/main/java/com/esprit/examen/controllers/ProduitRestController.java
@@ -1,12 +1,27 @@
 package com.esprit.examen.controllers;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
+import java.util.Set;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
+
+import com.esprit.examen.entities.CategorieProduit;
+import com.esprit.examen.entities.DetailFacture;
 import com.esprit.examen.entities.Produit;
+import com.esprit.examen.entities.Stock;
 import com.esprit.examen.services.IProduitService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import io.swagger.annotations.Api;
 
@@ -16,7 +31,17 @@ import io.swagger.annotations.Api;
 @Api(tags = "Gestion des produits")
 @RequestMapping("/produit")
 public class ProduitRestController {
-
+	public class ProduitRequestModel{
+		private Long idProduit;
+		private String codeProduit;
+		private String libelleProduit;
+		private float prix;
+		private Date dateCreation;
+		private Date dateDerniereModification;
+		private Stock stock;
+		private Set<DetailFacture> detailFacture;
+		private CategorieProduit categorieProduit;
+	}
 	@Autowired
 	IProduitService produitService;
 
@@ -35,7 +60,17 @@ public class ProduitRestController {
 
 	@PostMapping("/add-produit")
 	@ResponseBody
-	public Produit addProduit(@RequestBody Produit p) {
+	public Produit addProduit(@RequestBody ProduitRequestModel prod) {
+		Produit p = new Produit();
+		p.setCategorieProduit(prod.categorieProduit);
+		p.setCodeProduit(prod.codeProduit);
+		p.setDateCreation(prod.dateCreation);
+		p.setDateDerniereModification(prod.dateDerniereModification);
+		p.setDetailFacture(prod.detailFacture);
+		p.setIdProduit(p.getIdProduit());
+		p.setLibelleProduit(prod.libelleProduit);
+		p.setPrix(prod.prix);
+		p.setStock(prod.stock);
 		return produitService.addProduit(p);
 	}
 
@@ -48,7 +83,17 @@ public class ProduitRestController {
 	
 	@PutMapping("/modify-produit")
 	@ResponseBody
-	public Produit modifyProduit(@RequestBody Produit p) {
+	public Produit modifyProduit(@RequestBody ProduitRequestModel prod) {
+		Produit p = new Produit();
+		p.setCategorieProduit(prod.categorieProduit);
+		p.setCodeProduit(prod.codeProduit);
+		p.setDateCreation(prod.dateCreation);
+		p.setDateDerniereModification(prod.dateDerniereModification);
+		p.setDetailFacture(prod.detailFacture);
+		p.setIdProduit(p.getIdProduit());
+		p.setLibelleProduit(prod.libelleProduit);
+		p.setPrix(prod.prix);
+		p.setStock(prod.stock);
 		return produitService.updateProduit(p);
 	}
 
