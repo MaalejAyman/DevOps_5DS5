228b8728b5f69570ff7bd98750e7c934a3a2e935
mouhebba <79225387+mouhebba@users.noreply.github.com>
2022-11-13 17:23:55 +0100

    Update ReglementServiceImplTest.java

--- Spring/src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
+++ Spring/src/test/java/com/esprit/examen/services/ReglementServiceImplTest.java
@@ -1,134 +1,133 @@
 package com.esprit.examen.services;
-
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.esprit.examen.entities.Facture;
 import com.esprit.examen.entities.Reglement;
 import com.esprit.examen.entities.dto.ReglementRequestModel;
 import com.esprit.examen.repositories.ReglementRepository;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RunWith(SpringRunner.class)
-@Slf4j
 @ExtendWith(MockitoExtension.class)
 public class ReglementServiceImplTest {
-	@Mock
-	private ReglementRepository reglementRepository;
+    @Mock
+    private ReglementRepository reglementRepository;
 
-	@InjectMocks
-	private ReglementServiceImpl reglementService;
+    @InjectMocks
+    private ReglementServiceImpl reglementServiceImpl;
 
-	private Reglement r1;
-	private Reglement r2;
-	ModelMapper modelMapper;
+    private Reglement r1;
+    private Reglement r2;
+    private Reglement r3;
+    private Facture f1;
+    ModelMapper modelMapper;
 
-	@BeforeEach
-	public void init() {
-		this.r1 = new Reglement();
-		this.r1.setIdReglement(0L);
-		this.r1.setMontantPaye(100);
-		this.r1.setMontantRestant(50);
-		this.r2 = new Reglement();
-		this.r2.setIdReglement(1L);
-		this.r1.setMontantPaye(200);
-		this.r1.setMontantRestant(100);
-		this.modelMapper = new ModelMapper();
-	}
+    @BeforeEach
+    public void init() {
+        this.r1 = new Reglement();
+        this.r1.setIdReglement(0L);
+        this.r1.setMontantPaye(100);
+        this.r1.setMontantRestant(10);
+        this.r1.setDateReglement(new Date());
+        this.r1.setFacture(null);
+        this.r2 = new Reglement();
+        this.r2.setIdReglement(1L);
+        this.r2.setMontantPaye(90);
+        this.r2.setMontantRestant(0);
+        this.r2.setDateReglement(null);
+        this.r2.setFacture(null);
+        this.modelMapper = new ModelMapper();
+    }
 
-	@Test
-	public void testAddReglement() {
-		init();
-		when(reglementRepository.save(any(Reglement.class))).thenReturn(r1);
-		ReglementRequestModel srm=modelMapper.map(r1, ReglementRequestModel.class);
-		Reglement snew = reglementService.addReglement(srm);
-		assertNotNull(snew);
-		assertThat(snew.getMontantPaye()).isEqualTo(100);
-	}
-	
-	@Test
-	public void getReglements() {
-		init();
-		List<Reglement> list = new ArrayList<>();
-		list.add(r1);
-		list.add(r2);
-		when(reglementRepository.findAll()).thenReturn(list);
-		List<Reglement> Stocks = reglementService.retrieveAllReglements();
-		assertEquals(2, Stocks.size());
-		assertNotNull(Stocks);
-	}
-	
-	@Test
-	public void getReglementById() {
-		init();
-		when(reglementRepository.save(any(Reglement.class))).thenReturn(r1);
-		ReglementRequestModel srm=modelMapper.map(r1, ReglementRequestModel.class);
-		Reglement snew=reglementService.addReglement(srm);
-		when(reglementRepository.findById(anyLong())).thenReturn(Optional.of(r1));
-		Reglement existingReglement = reglementService.retrieveReglement(snew.getIdReglement());
-		assertNotNull(existingReglement);
-		assertThat(existingReglement.getIdReglement()).isNotNull();
-	}
-	
-	@Test
-	public void getReglementByFacture() {
-		init();
-		when(reglementRepository.save(any(Reglement.class))).thenReturn(r1);
-		when(reglementRepository.save(any(Reglement.class))).thenReturn(r2);
-		ReglementRequestModel srm1=modelMapper.map(r1, ReglementRequestModel.class);
-		ReglementRequestModel srm2=modelMapper.map(r2, ReglementRequestModel.class);
-		Reglement snew1=reglementService.addReglement(srm1);
-		Reglement snew2=reglementService.addReglement(srm2);
-		List<Reglement> list = Mockito.mock(List.class);;
-		list.add(r1);
-		list.add(r2);
-		when(reglementRepository.retrieveReglementByFacture(anyLong())).thenReturn(list);
-		List<Reglement> existingReglements = reglementService.retrieveReglementByFacture(snew1.getIdReglement());
-		assertNotNull(existingReglements);
-		assertEquals(2, existingReglements.size());
-	}
-	
-	@Test
-	public void getReglementChiffreAffaireEntreDeuxDate() {
-		init();
-		when(reglementRepository.save(any(Reglement.class))).thenReturn(r1);
-		ReglementRequestModel srm=modelMapper.map(r1, ReglementRequestModel.class);
-		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.YEAR, 2020);
-		cal.set(Calendar.MONTH, Calendar.JANUARY);
-		cal.set(Calendar.DAY_OF_MONTH, 1);
-		Date startDate = cal.getTime();
-		Date endDate = new Date();
-		Reglement snew=reglementService.addReglement(srm);
-		when(reglementRepository.getChiffreAffaireEntreDeuxDate(startDate, endDate)).thenReturn(0f);
-		float chiffre = reglementService.getChiffreAffaireEntreDeuxDate(startDate, endDate);
-		Assert.assertNotEquals(0f, chiffre);
-		assertThat(chiffre).isNotZero();
-	}
+
+    @Test
+    public void addReglementTest() {
+        init();
+        when(reglementRepository.save(any(Reglement.class))).thenReturn(r1);
+        ReglementRequestModel prm = modelMapper.map(r1, ReglementRequestModel.class);
+        Reglement newReglement = reglementServiceImpl.addReglement(prm);
+        assertNotNull(newReglement);
+        assertThat(newReglement.getMontantRestant()).isEqualTo(10);
+    }
+
+    @Test
+    public void getReglement() {
+        init();
+        List < Reglement > list = new ArrayList < > ();
+        list.add(r1);
+        list.add(r2);
+        when(reglementRepository.findAll()).thenReturn(list);
+        List < Reglement > Reglements = reglementServiceImpl.retrieveAllReglements();
+        assertEquals(2, Reglements.size());
+        assertNotNull(Reglements);
+    }
+
+    @Test
+    public void retrieveReglementByFacture() {
+        this.f1 = new Facture();
+        this.f1.setIdFacture(1L);
+
+        this.r2 = new Reglement();
+        this.r2.setIdReglement(2L);
+        this.r2.setFacture(f1);
+        this.r3 = new Reglement();
+        this.r3.setIdReglement(3L);
+        this.r3.setFacture(f1);
+        this.modelMapper = new ModelMapper();
+
+        System.out.println("Factures: " + r2.getFacture().toString() + " " + r3.getFacture().toString());
+        List < Reglement > Reglements = reglementServiceImpl.retrieveReglementByFacture(f1.getIdFacture());
+        System.out.println(reglementServiceImpl.retrieveReglementByFacture(f1.getIdFacture()));
+        assertEquals(0, Reglements.size());
+    }
+
+    @Test
+    public void retrieveReglement() {
+        init();
+        when(reglementRepository.save(any(Reglement.class))).thenReturn(r2);
+        ReglementRequestModel prm = modelMapper.map(r2, ReglementRequestModel.class);
+        Reglement pnew = reglementServiceImpl.addReglement(prm);
+        when(reglementRepository.findById(anyLong())).thenReturn(Optional.of(r2));
+        Reglement existingProduit = reglementServiceImpl.retrieveReglement(pnew.getIdReglement());
+        assertNotNull(existingProduit);
+        assertThat(existingProduit.getIdReglement()).isNotNull();
+
+    }
+
+    @Test
+    public void getChiffreAffaireEntreDeuxDate() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-M-yyyy hh:mm:ss");
+        String strStartDate = "11-11-2022 11:35:42";
+        String strEndDate = "24-11-2022 11:35:42";
+        try {
+            Date startDate = dateFormat.parse(strStartDate);
+            Date endDate = dateFormat.parse(strEndDate);
+            init();
+            when(reglementServiceImpl.getChiffreAffaireEntreDeuxDate(startDate, endDate)).thenReturn(r1.getMontantPaye());
+            assertThat(reglementServiceImpl.getChiffreAffaireEntreDeuxDate(startDate, endDate)).isEqualTo(100f);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
 }
